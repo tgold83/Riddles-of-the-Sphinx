09a0fbe46ba0d23950cad3ecfe7736595df3d275
tgold83 <ted.jonathan.gold@gmail.com>
2022-09-19 14:54:04 -0700

    add random function to create new list order of questions
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>

--- Program.cs
+++ Program.cs
@@ -33,13 +33,31 @@ namespace Riddles
       Riddle age = new Riddle("What goes up, but never comes down?", "age");
       Riddle towel = new Riddle("What gets wet as it dries?", "towel");
       Riddle future = new Riddle("What is always in front of you but can't be seen?", "future");
-      if (ask.askRiddle(age))
+      Riddle shirt = new Riddle("What has a neck but no head?", "shirt");
+      Riddle death = new Riddle("Some try to hide, some try to cheat; but time will show, we always will meet. Try as you might to guess my name.", "death");
+      List<Riddle> list1 = new List<Riddle> {age, towel, future, shirt, death};
+      List<Riddle> list2 = new List<Riddle> {};
+      int constnum = list1.Count;
+      for (int x = 0; x < constnum; x++)
       {
-        if (ask.askRiddle(towel))
+        Random rnd = new Random();
+        int rndnum = rnd.Next(0, constnum - x);
+        list2.Add(list1[rndnum]);
+        list1.Remove(list1[rndnum]);
+      }
+      if (ask.askRiddle(list2[0]))
+      {
+        if (ask.askRiddle(list2[1]))
         {
-          if (ask.askRiddle(future))
+          if (ask.askRiddle(list2[2]))
           {
-            Console.WriteLine("You have answered all the questions correctly and defeated the Mighty Sphinx!");
+            if (ask.askRiddle(list2[3]))
+            {
+              if (ask.askRiddle(list2[4]))
+              {
+                Console.WriteLine("You have answered all the questions correctly and defeated the Mighty Sphinx!");
+              }
+            }
           }
         }
       }
